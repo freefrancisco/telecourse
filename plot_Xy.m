1f2a9d1a761442b3a1a810e70a7df59b392e74dd
Francisco Gutierrez <freefrancisco@gmail.com>
2013-01-20 11:26:59 -0800

    some more graphics improvements

--- plot_Xy.m
+++ plot_Xy.m
@@ -2,6 +2,6 @@ function plot_Xy(X, y)
   graphics_toolkit ("fltk")
   figure(1)
   hold off
-  scatter(X(:,2), X(:,3), 20, y)
+  scatter(X(:,2), X(:,3), 10, y, "filled")
   axis([-1.2, 1.2, -1.2, 1.2], "square")
 end

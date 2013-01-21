409fe9e465d48a42713ca177adb0e3ed1db346c8
Francisco Gutierrez <freefrancisco@gmail.com>
2013-01-21 07:16:15 -0800

    improved generate data and plot w

--- generate_data.m
+++ generate_data.m
@@ -1,10 +1,7 @@
 function [X, y, w] = generate_data(N, classify=false)
-  X = [ones(N,1), 2*rand(N, 2) - 1]; %100 column vectors
+  X = [ones(N,1), 2*rand(N, 2) - 1]; % N random row vectors
   p = 2*rand(2) - 1; %two points to define a line as separator
-  w = planerot([0,1]) * (p(1,:) - p(2,:))'; % rotate vector 90 degrees so it's orthogonal to line
-  w = w / norm(w); %normalize so we have a unit vector
-  w0 = -dot(w, p(1,:)); % distance of line from origin, bias point is negative of that
-  w = [w0; w]; %complete set of weights
+  w = plane_from_points(p); %find the w that determines the line, [1,p]*w = 0, using rows as points
   y = X*w;
   if(classify)
     y = sign(y);
--- plot_w.m
+++ plot_w.m
@@ -1,10 +1,12 @@
-function h = plot_w(w)
+function h = plot_w(w, color="black", normal=false)
   figure(1)
   hold on
   h = ezplot(@(x, y) w(1) + w(2)*x + w(3)*y, [-1.2, 1.2, -1.2, 1.2]);
   set(h, "linewidth", 4);
-  set(h, "color", "black");
-  plot([0,w(2)], [0,w(3)], 'g-')
-  scatter([0,w(2)], [0,w(3)], 20, 0)
+  set(h, "color", color);
+  if(normal)
+    plot([0,w(2)], [0,w(3)], 'g-')
+    scatter([0,w(2)], [0,w(3)], 20, 0)
+  end
   hold off
 end
